12881e672d1716ca35a487354929970b1874695f
nisaefendioglu <nisa1517@gmail.com>
2021-08-30 12:38:41 +0300

    navbar added.

--- app/src/main/java/com/nisaefendioglu/androidsystemapp/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/androidsystemapp/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setItemIconTintList(null);
     }
 }
